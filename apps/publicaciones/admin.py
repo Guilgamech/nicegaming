f1dd3d0ce8a35fce37e22d146b6a6b1548e44ff1
VRF <josemanuel22072000@gmail.com>
2022-02-02 13:30:52 -0500

    subiendo el el db

--- apps/publicaciones/admin.py
+++ apps/publicaciones/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from apps.publicaciones.models import *
+
+admin.site.register(Autor)
+admin.site.register(Post)
+admin.site.register(Categoria)
+admin.site.register(Informacion)
+admin.site.register(RedesSociales)
+admin.site.register(Contacto)
+admin.site.register(Suscripcion)
